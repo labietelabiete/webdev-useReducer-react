ac60b93d9db7b0f4c32bda4e314ce8720b2a05b3
labietelabiete <victorhugogote@gmail.com>
2021-06-03 18:58:22 +0200

    counter with reducer done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React, { useState } from 'react';
+import React, { useReducer } from "react";
+
+const ACTIONS = {
+	INCREMENT: 'increment',
+	DECREMENT: 'decrement'
+}
+
+function reducer(state, action) {
+	switch (action.type) {
+		case ACTIONS.INCREMENT:
+			return { count: state.count + 1 }
+		case ACTIONS.DECREMENT:
+			return { count: state.count - 1 }
+		default:
+			return state
+	}
+}
 
 export default function App() {
-	const [count, setCount] = useState(0);
+	const [state, dispatch] = useReducer(reducer, { count: 0 })
 
 	function increment() {
-		setCount(prevCount => prevCount + 1)
+		dispatch({ type: ACTIONS.INCREMENT });
 	}
 
 	function decrement() {
-		setCount(prevCount => prevCount - 1)
+		dispatch({ type: ACTIONS.DECREMENT });
 	}
 
 	return (
 		<>
-			<button onclick={decrement}>-</button>
-			<span>{count}</span>
-			<button onclick={increment}>+</button>
+			<button onClick={decrement}>-</button>
+			<span>{state.count}</span>
+			<button onClick={increment}>+</button>
 		</>
 	)
 }
